297476bc5ad6df7e46ea2b52a3a2f0f679b1c2d0
Isaac <isaac@foundry.ai>
2020-10-22 18:33:44 -0400

    Navigate to URL and click.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from playwright import sync_playwright
 
-with sync_playwright() as p:
-    browser_type = p.firefox
-    browser = browser_type.launch()
+with sync_playwright() as playwright:
+    browser = playwright.firefox.launch()
     page = browser.newPage()
-    page.goto("http://whatsmyuseragent.org")
-    page.screenshot(path=f"/img/example-{browser_type.name}.png")
+
+    page.goto("https://funhtml5games.com/helicopter/index.html")
+    page.waitForSelector("canvas")
+
+    start = datetime.now()
+    page.click("canvas", delay=1000)
+    end = datetime.now()
+    score = (end - start).seconds
+
+    page.querySelector("canvas").screenshot(path="/img/helicopter.png")
     browser.close()
